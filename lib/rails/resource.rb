e6ef4ba738a6e951e43c879bceea7ddd0d3f30cc
Ingo Weiss <ingo@ingoweiss.com>
2010-04-13 18:49:56 +0200

    Added some more resource attributes

--- lib/rails/resource.rb
+++ lib/rails/resource.rb
@@ -1,14 +1,16 @@
 module Rails
   class Resource
     
-    attr_accessor :name, :controller
+    attr_accessor :name, :singular_name, :id, :route, :controller_name, :controller, :name_prefix
     
     def initialize(entity, scope, options)
-      self.name = [scope[:name_prefix], entity].compact.join('_')
-    end
-    
-    def controller
-      @controller ||= (Rails.application.routes.named_routes[name].defaults[:controller] + '_controller').camelize.constantize
+      self.name_prefix = scope[:name_prefix]
+      self.name = entity.to_s
+      self.singular_name = name.singularize
+      self.id = [name_prefix, name].compact.join('_')
+      self.route = Rails.application.routes.named_routes[id]
+      self.controller_name = route.defaults[:controller]
+      self.controller = (controller_name + '_controller').camelize.constantize
     end
     
   end
--- test/resource_awareness_test.rb
+++ test/resource_awareness_test.rb
@@ -13,29 +13,35 @@ class ResourceAwarenessTest < Test::Unit::TestCase
   end
   
   def test_resources_should_know_their_controller
-    assert_equal PostsController,         find_resource_by_name('posts').controller
-    assert_equal PostCommentsController,  find_resource_by_name('post_comments').controller
-    assert_equal PostApprovalController,  find_resource_by_name('post_approval').controller
+    assert_equal PostsController,         find_resource('posts').controller
+    assert_equal PostCommentsController,  find_resource('post_comments').controller
+    assert_equal PostApprovalController,  find_resource('post_approval').controller
   end
   
   def test_controllers_should_know_their_resource
-    assert_equal find_resource_by_name('posts'),          PostsController.resource
-    assert_equal find_resource_by_name('post_comments'),  PostCommentsController.resource
-    assert_equal find_resource_by_name('post_approval'),  PostApprovalController.resource
+    assert_equal find_resource('posts'),          PostsController.resource
+    assert_equal find_resource('post_comments'),  PostCommentsController.resource
+    assert_equal find_resource('post_approval'),  PostApprovalController.resource
+  end
+  
+  def test_resources_should_know_their_name_prefix
+    assert_equal nil, find_resource('posts').name_prefix
+    assert_equal 'post', find_resource('post_comments').name_prefix
+    assert_equal 'post', find_resource('post_approval').name_prefix
   end
   
   private
   
-  def assert_resource_known(resource_name, resource_class=Rails::Resource)
-    assert_not_nil find_resource_by_name(resource_name, resource_class), "No resource with name #{resource_name} known"
+  def assert_resource_known(resource_id, resource_class=Rails::Resource)
+    assert_not_nil find_resource(resource_id, resource_class), "No resource with id #{resource_id} known"
   end
   
-  def assert_singleton_resource_known(resource_name)
-    assert_resource_known(resource_name, Rails::SingletonResource)
+  def assert_singleton_resource_known(resource_id)
+    assert_resource_known(resource_id, Rails::SingletonResource)
   end
   
-  def find_resource_by_name(resource_name, resource_class=Rails::Resource)
-    Rails.application.resources.find { |r| r.is_a?(resource_class) && r.name == resource_name }
+  def find_resource(resource_id, resource_class=Rails::Resource)
+    Rails.application.resources.find { |r| r.is_a?(resource_class) && r.id == resource_id }
   end
   
 end
